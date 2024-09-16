bf9aff1ec33195b04b00d22f3caaf1bb142019e2
arss5 <adtr.santos@gmail.com>
2024-09-15 23:54:37 -0300

    Chapter 6 Done

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main() {
     Tuple ray_origin = Point(0, 0, -5);
     float wall_z = 10;
     float wall_size = 7;
-    int canvas_pixels = 1000;
+    int canvas_pixels = 200;
     float pixel_size = wall_size / canvas_pixels;
     float half = wall_size / 2;
 
@@ -19,6 +19,12 @@ int main() {
     // canvas.set_color(Color(0, 1, 1));
     Color color = Color(1, 0, 0);
     Sphere s = Sphere((Point(0, 0, 0)), 1);
+    s.material = Material();
+    s.material.color = Color(0.5, 0.2, 1);
+    s.material.shininess = 120;
+    Tuple light_postion = Point(-10, 10, -10);
+    Color light_color (1, 1, 1);
+    PointLight light = PointLight(light_postion, light_color);
 
     float world_y;
     float world_x;
@@ -29,12 +35,16 @@ int main() {
         for(int x=0; x < canvas_pixels; x++){
             world_x = -half + pixel_size * x;
             Tuple position = Point(world_x, world_y, wall_z);
-            Tuple test = normalize(position - ray_origin);
-            r = Ray(ray_origin, test);
+            Tuple direction_normal = normalize(position - ray_origin);
+            r = Ray(ray_origin, direction_normal);
             xs = intersect(s, r);
             if (xs.is_empty()){
                 continue;
             }else if (hit(xs).t > 0){
+                Tuple point = r.position(hit(xs).t);
+                Tuple normal = normal_at(s, point); //Esse S precisa ser o objeto que foi atingido (hit(xs).object)
+                Tuple eye = -r.direction;
+                color = lighting(s.material, light, point, eye, normal); //Aqui também
                 canvas.write_pixel(x, y, color);
             }
         }
